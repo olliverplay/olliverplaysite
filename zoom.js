5038d4326cad961e5cc94ee4234a918a418de52c
William oliveira da silva <94722956+olliverplay@users.noreply.github.com>
2022-07-11 21:40:00 -0300

    olliverplay

---
+++ zoom.js
@@ -0,0 +1,16 @@
+const card1 = document.getElementById("card1")
+const img = document.getElementById("img")
+
+    card1.addEventListener("mousemove", (e) =>{
+        const x = e.clientX - e.target.offsetLef;
+        const y = e.clientY - e.target.offsetTop;
+
+        img.style.transformOrigin = `${x}px ${y}px`;
+        img.style.transform = "scale(2)";
+        img.style.display = "inline";
+    })
+
+    card1.addEventListener("mousemove", ()=>{
+        img.style.transformOrigin = "center center";
+        img.style.transform = "scale(1)"    
+    })
